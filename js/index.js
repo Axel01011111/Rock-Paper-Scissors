516f843f3aa3668da9871926a6ddbad527116ac9
Axel <53453024+Axel01011111@users.noreply.github.com>
2020-02-11 00:08:16 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,22 +1,43 @@
+//function variables
 var buttons = document.getElementsByClassName("btn");
 var playerSelect;
-var randomNum = Math.floor(Math.random() * 3);
 
+var gameStart = false;
+
+//What players choose
 var human;
 var computer;
 
-for (var i = 0; i < buttons.length; i++) {
-  document.querySelectorAll(".btn")[i].addEventListener("click", function() {
-    secondScene();
-    playerSelect = event.target;
-    playerSelection();
-    setTimeout(function() {
-      computerSelection(randomNum)
-    }, 2000);
+//score
+var humanScore = 0;
 
-    setTimeout(function() {
-      whoWin(human, computer);
-    }, 3000);
+
+//MAIN GAME
+for (var i = 0; i < buttons.length; i++) {
+  score();
+  document.querySelectorAll(".btn")[i].addEventListener("click", function() {
+    //starting game
+    gameStart = true;
+
+    //setting variables
+    var randomNum = Math.floor(Math.random() * 3);
+
+
+    if (gameStart == true) {
+      secondScene();
+      playerSelect = event.target;
+      playerSelection();
+      setTimeout(function() {
+        computerSelection(randomNum)
+      }, 2000);
+
+      setTimeout(function() {
+        whoWin(human, computer);
+      }, 3000);
+
+      reset();
+    }
+
   });
 }
 
@@ -24,6 +45,27 @@ for (var i = 0; i < buttons.length; i++) {
 
 
 /*---FUNCTIONS---*/
+function reset() {
+  document.querySelector(".play-again").addEventListener("click", function() {
+    //reseting window
+    document.querySelector(".container").style.display = "block";
+    document.querySelector(".second-scene").style.display = "none";
+    gameStart = false;
+
+    //Reseting players
+    document.querySelector("#computer-pick").className = "loading-round";
+    document.querySelector("#player-pick").className = "";
+
+    //Reseting results
+    document.querySelector(".play-again").style.display = "none";
+    document.querySelector(".decision").innerHTML = "";
+  });
+}
+
+function score() {
+  document.querySelector(".score-points").innerHTML = humanScore;
+}
+
 function secondScene() {
   document.querySelector(".container").style.display = "none";
   document.querySelector(".second-scene").style.display = "block";
@@ -73,19 +115,32 @@ function computerSelection(num) {
 function whoWin(num1, num2) {
   if (num1 == 0 && num2 == 1) {
     document.querySelector(".decision").innerHTML = "You Lose";
+    document.querySelector("#computer-pick").classList.add("aura");
+    humanScore--;
   } else if (num1 == 0 && num2 == 2) {
     document.querySelector(".decision").innerHTML = "You Win";
+    document.querySelector("#player-pick").classList.add("aura");
+    humanScore++;
   } else if (num1 == 1 && num2 == 0) {
     document.querySelector(".decision").innerHTML = "You Win";
+    document.querySelector("#player-pick").classList.add("aura");
+    humanScore++;
   } else if (num1 == 1 && num2 == 2) {
     document.querySelector(".decision").innerHTML = "You Lose";
+    document.querySelector("#computer-pick").classList.add("aura");
+    humanScore--;
   } else if (num1 == 2 && num2 == 0) {
     document.querySelector(".decision").innerHTML = "You Lose";
+    document.querySelector("#computer-pick").classList.add("aura");
+    humanScore--;
   } else if (num1 == 2 && num2 == 1) {
     document.querySelector(".decision").innerHTML = "You Win";
+    document.querySelector("#player-pick").classList.add("aura");
+    humanScore++;
   } else {
     document.querySelector(".decision").innerHTML = "Draw";
   }
+  score();
   document.querySelector(".play-again").innerHTML = "Play again";
   document.querySelector(".play-again").style.display = "block";
 }
